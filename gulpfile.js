d0b1169ec41811d94c4181e6ea18768915a722a1
Jeff <jgomez630@aol.com>
2018-03-28 00:22:02 -0700

    added base, global, styles css files

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 
 gulp.task('default', function(){
-	console.log('Th Default task is working');
+	console.log('The Default task is working');
 });
 
 gulp.task('html', function(){
 	console.log('HTML task is ok');
+});
+gulp.task('styles', function(){
+  console.log('css sheet updated');
 });
 
 gulp.task('watch', function(){
@@ -14,6 +17,9 @@ gulp.task('watch', function(){
 	watch('./app/index.html', function(){
 		gulp.start('html');
 	});
+  watch('./app/assets/styles/**/*.css', function(){
+      gulp.start('styles');
+  });
 
 });
 /*added a comment*/
