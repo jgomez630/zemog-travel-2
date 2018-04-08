9eb3c58ec5db5ac5725747af1ebe1798becf5433
Jeff <jgomez630@aol.com>
2018-04-08 12:22:57 -0700

    complete ch.15

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function(){
 	console.log('The Default task is working');
@@ -9,7 +14,8 @@ gulp.task('html', function(){
 	console.log('HTML task is ok');
 });
 gulp.task('styles', function(){
-  return gulp.source('./app/assets/styles/styles.css').
+  return gulp.src('./app/assets/styles/styles.css')
+   .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))	
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
