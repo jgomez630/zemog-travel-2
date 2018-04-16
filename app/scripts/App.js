0ac43516c4c7a77a7f2d1ac84a5ae3ae2d3126f5
Jeff <jgomez630@aol.com>
2018-04-16 14:05:27 -0700

    Finished ch.24 Column layout block

---
+++ app/scripts/App.js
@@ -0,0 +1,5 @@
+var john = new Person ('John Doe', 'blue');
+john.greet();
+
+var janne = new Person ('Janne Diaz', 'red');
+janne.name;
---
+++ app/scripts/modules/Person.js
@@ -0,0 +1,7 @@
+function Person (fullName, favColor) {
+	this.name = fullName;
+	this.favoriteColor = favColor;
+	this.greet = function () {
+		console.log("My name is " + this.name + " and my favorite color is " + this.favoriteColor  );
+	}
+}
